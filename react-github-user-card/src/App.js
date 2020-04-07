9b8a730e5f486748ce853e8a5715427d80885725
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-07 16:46:53 -0400

    Have both axios requests working and have two states set

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      followers: [],
+    };
+  }
+
+  componentDidMount() {
+    Axios.get("https://api.github.com/users/mrsimpson3000")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          user: response.data,
+        });
+        console.log(this.state.user.login);
+      })
+      .catch((error) => {
+        console.log(`There was an error: ${error}`);
+      });
+
+    Axios.get("https://api.github.com/users/mrsimpson3000/followers")
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({
+          followers: response.data, // This is an array of objects
+        });
+        // console.log(this.state.followers);
+      })
+      .catch((error) => {
+        console.log(`There was an error: ${error}`);
+      });
+  }
+
+  render() {
+    return null;
+  }
 }
 
 export default App;
